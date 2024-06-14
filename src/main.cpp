af12ff4aa671e54947dd8a214fbdbe0d36cc23fe
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2024-06-14 16:57:13 -0400

    Added multithreaded support

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,27 @@
 #include <cctype>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <vector>
 
 #include "card.h"
 #include "deck.h"
 #include "game.h"
 #include "player.h"
 
+/// @struct SimResults
+/// @brief Struct to hold the Game results from a given threads runs
+typedef struct
+{
+    int p1; //!< Player 1 wins
+    int p2; //!< Player 2 wins
+    int t;  //!< Ties
+} SimResults;
+std::vector<SimResults> sr;
+
+/// @brief Convert the given string to lower case and return the result
+/// @param s The string to convert to lower case
+/// @return The string in all lower case
 std::string strLower(std::string s)
 {
     std::transform(s.begin(), s.end(), s.begin(),
@@ -15,9 +30,104 @@ std::string strLower(std::string s)
     return s;
 }
 
+/// @brief The main sim loop for running games of war
+/// @param simulated Should the Game be simulated or interactive
+/// @param rigged Should the Game be rigged for Player 1
+/// @param verbose Should a turn-by-turn summary be printed
+/// @return The result of the game
+int simLoop(bool simulated, bool rigged, bool verbose)
+{
+    Player p1, p2;
+    Deck d;
+
+    d.dealCards(p1, p2);
+    if (rigged)
+        d.rigDeck(p1, p2);
+
+    Game g = Game(p1, p2, simulated, verbose);
+    g.play();
+
+    return g.getWinner();
+}
+
+/// @brief Function for threads running the simulation loop
+/// @param rigged Should the Game be rigged for Player 1
+/// @param sims The number of simulations this thread is running
+/// @param id The id of the thread and corresponding SimResults
+void threadFunc(bool rigged, int sims, int id)
+{
+    while (sims-- > 0)
+    {
+        switch (simLoop(true, rigged, false))
+        {
+            case GAME_RESULT_TIE:
+                sr[id].t++;
+                break;
+            case GAME_RESULT_P1_W:
+                sr[id].p1++;
+                break;
+            case GAME_RESULT_P2_W:
+                sr[id].p2++;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+/// @brief Create, and start, the threads to run the war simulation loop
+/// @param rounds The number of simulations to run
+/// @param rigged Should the Game be rigged for Player 1
+void startThreads(int rounds, bool rigged)
+{
+    const auto cores = std::min(rounds,
+                                (int) std::thread::hardware_concurrency());
+    std::vector<std::thread> threads(cores);
+    int perThread = rounds / cores;
+    int extra = rounds % cores;
+
+    for (int t = 0; t < (int) cores; t++)
+    {
+        int sims = perThread;
+        if (extra > 0)
+        {
+            extra--;
+            sims++;
+        }
+
+        sr.emplace_back(SimResults());
+        threads[t] = std::thread(threadFunc, rigged, sims, t);
+    }
+
+    for (int t = 0; t < (int) cores; t++)
+        threads[t].join();
+}
+
+/// @brief Output the results of the War Game(s) to the console
+/// @param rounds The number of simulations to run
+/// @param p1 Number of wins for Player 1
+/// @param p2 Number of wins for Player 2
+/// @param t Number of ties
+void outputResults(int rounds, int p1, int p2, int t)
+{
+    const int pad = 8;
+    printf("%sStats:\n", (rounds == 1) ? "\n" : "");
+    printf("%*s%*s%*s%*s\n", pad, "Player", pad, "Wins", pad, "Losses", pad,
+           "Ties");
+    printf("%*s%*d%*d%*d\n", pad, "Player 1", pad, p1, pad, (rounds - p1) - t,
+           pad, t);
+    printf("%*s%*d%*d%*d\n", pad, "Player 2", pad, p2, pad, (rounds - p2) - t,
+           pad, t);
+    printf("Overall Winner: ");
+    if (p1 == p2)
+        printf("Tie\n");
+    else
+        printf("%s\n", (p1 > p2) ? "Player 1" : "Player 2");
+}
+
 int main(int argc, char **argv)
 {
-    bool simulated = true, rigged = false;
+    bool simulated = true, rigged = false, threadded = false;
     int rounds = 1;
 
     for (int x = 1; x < argc; x++)
@@ -47,57 +157,56 @@ int main(int argc, char **argv)
                 return 1;
             }
         }
+        else if (arg == "-t")
+            threadded = true;
         else if (arg == "-h" || arg == "--help")
         {
-            printf("Usage: %s [-m] [-r] [-s sims]\n", argv[0]);
+            printf("Usage: %s [-m] [-r] [-s sims] [-t]\n", argv[0]);
             printf("  -m       Enable manual mode and step through the game "
                    "turn-by-turn\n");
             printf("  -r       Rig the deck in favor of Player 1\n");
             printf("  -s sims  The number of simulations to run\n");
+            printf("  -t       Run in multithreaded mode\n");
             printf("\n");
             return 0;
         }
     }
 
     int p1Wins = 0, p2Wins = 0, ties = 0;
-    for (int x = 0; x < rounds; x++)
+    if (threadded && rounds > 1)
     {
-        Player p1, p2;
-        Deck d;
-        d.dealCards(p1, p2);
-        if (rigged)
-            d.rigDeck(p1, p2);
-
-        Game g = Game(p1, p2, simulated, (rounds == 1));
-        g.play();
-        switch (g.getWinner())
+        startThreads(rounds, rigged);
+        for (int x = 0; x < (int) sr.size(); x++)
         {
-            case GAME_RESULT_TIE:
-                ties++;
-                break;
-            case GAME_RESULT_P1_W:
-                p1Wins++;
-                break;
-            case GAME_RESULT_P2_W:
-                p2Wins++;
-                break;
-            default:
-                break;
+            p1Wins += sr[x].p1;
+            p2Wins += sr[x].p2;
+            ties += sr[x].t;
         }
     }
-    const int pad = 8;
-    printf("%sStats:\n", (rounds == 1) ? "\n" : "");
-    printf("%*s%*s%*s%*s\n", pad, "Player", pad, "Wins", pad, "Losses", pad,
-           "Ties");
-    printf("%*s%*d%*d%*d\n", pad, "Player 1", pad, p1Wins, pad,
-           (rounds - p1Wins) - ties, pad, ties);
-    printf("%*s%*d%*d%*d\n", pad, "Player 2", pad, p2Wins, pad,
-           (rounds - p2Wins) - ties, pad, ties);
-    printf("Overall Winner: ");
-    if (p1Wins == p2Wins)
-        printf("Tie\n");
     else
-        printf("%s\n", (p1Wins > p2Wins) ? "Player 1" : "Player 2");
+    {
+        int remaining = rounds;
+        while (remaining > 0)
+        {
+            switch (simLoop(simulated, rigged, (rounds == 1)))
+            {
+                case GAME_RESULT_TIE:
+                    ties++;
+                    break;
+                case GAME_RESULT_P1_W:
+                    p1Wins++;
+                    break;
+                case GAME_RESULT_P2_W:
+                    p2Wins++;
+                    break;
+                default:
+                    break;
+            }
+            remaining--;
+        }
+    }
+
+    outputResults(rounds, p1Wins, p2Wins, ties);
 
     return 0;
 }
